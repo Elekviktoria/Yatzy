06a420c70430ceb41e77de6c3145a29df23be1aa
Elekviktoria <114669450+Elekviktoria@users.noreply.github.com>
2023-03-31 09:41:53 +0200

    Ändrade allt, började med klasser. Input-listan är samma, fast i en funktion.

--- Yatzy.py
+++ Yatzy.py
@@ -1,13 +1,22 @@
 import numpy as np
-
-
-def dice():
-    for i in range(3):
-        dice = np.random.randint(1,7, size=5)
-        print("Dice: ({},{},{},{},{})".format(dice[0], dice[1], dice[2], dice[3], dice[4]))
-        index = list(map(int, input("\nEnter the numbers : ").strip().split()))[:5]
-  
-        print("\nList is - ", index)
         
+class DICE:
+    
+    def __init__(self, number:int, värde:int):
+        self.number=number
+        self.värde=värde
         
-dice()
+    def roll_dice(self):
+        self.värde = np.random.randint(1,7)
+        print(self.värde)
+
+def ask():
+    index = list(map(int, input("\nEnter the numbers 0 - 4: ").strip().split()))[:5]
+    print("\nList is - ", index)
+ask()
+    
+dice1 = DICE(1, 0)
+dice2 = DICE(2, 0)
+dice3 = DICE(3, 0)
+dice4 = DICE(4, 0)
+dice5 = DICE(5, 0)
