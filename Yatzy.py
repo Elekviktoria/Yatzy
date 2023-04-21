6c0a6aeaa5ed7dda4ad49c1781ee2c8a57a5cf53
Elekviktoria <114669450+Elekviktoria@users.noreply.github.com>
2023-04-21 09:55:58 +0200

    Fixade så att funktionen för rullandet av tärningarna initieras genom konstruktör. Lade till en array för tärningarna för varje spelare, lagt in dictionaries för de olika poängen, detta är inte färdigt. Längst ner finns början till en input som ska låsa tärningarna.

--- Yatzy.py
+++ Yatzy.py
@@ -1,40 +1,43 @@
 import numpy as np
-        
-class DICE:
     
-    def __init__(self, number:int, värde:int):
-        self.number=number
-        self.värde=värde
-        
-    def roll_dice(self):
-        self.värde = np.random.randint(1,7)
-        print(self.värde)
-
-def ask():
-    index = list(map(int, input("\nEnter the numbers 0 - 4: ").strip().split()))[:5]
-    print("\nList is - ", index)
-ask()
+class DIE:
     
-dice1 = DICE(1, 0)
-dice2 = DICE(2, 0)
-dice3 = DICE(3, 0)
-dice4 = DICE(4, 0)
-dice5 = DICE(5, 0)
+    value = 7
+    hold = False
+    
+    def __init__(self, namn: str):
+        self.namn=namn
+        self.roll_die()
+        
+        
+    def roll_die(self):
+        if self.hold == False:
+            self.value = np.random.randint(1,7)
+            print(self.value)
+        else:
+            pass
 
-import numpy as np
-
-def dice():
-        dice = np.random.randint(1,7, size=5)
-        print("Dice: ({},{},{},{},{})".format(dice[0], dice[1], dice[2], dice[3], dice[4]))
+class PLAYERS:
+    
+    antal_spelare = 0
+    dices = np.array([DIE("d1"), DIE("d2"), DIE("d3"), DIE("d4"), DIE("d5")])
+    points_siffror = {"ettor":0, "tvåor":0, "treor":0, "fyror":0, "femmor":0, "sexor":0}
+    points_extra = {"tretal":0, "fyrtal":0, "liten stege":0, "stor stege":0, "kåk":0, "chans":0, "yatzy":0}
+    
+    def __init__(self,name):
+        self.name=name
+    
+        
+    def antal(self):
+        antal_spelare = input(int("Hur många spelare?"))
+        
+    def getname(self):
+        for i in range(antal_spelare):
+            self.name = input(str("What is your name?"))
+        
+class START:
+    def __init__(self):
+        
+    def ask():
         index = list(map(int, input("\nEnter the numbers 0 - 4: ").strip().split()))[:5]
-  
         print("\nList is - ", index)
-    
-        for i in range(index):
-            if index[i] == 
-
-start = input("Start? ")       
-
-if start == "yes":
-    dice()
-    
