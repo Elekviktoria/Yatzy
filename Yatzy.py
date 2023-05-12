50fe0a5589035b3498708fd4c9f9df7eedc4e799
Elekviktoria <114669450+Elekviktoria@users.noreply.github.com>
2023-05-12 09:43:47 +0200

    tillverkade ett litet "skelett" för koden som ska göra poängen. Ny klass så att "changeability" kan kollas, så att man inte kan skriva över sina gamla poäng.

--- Yatzy.py
+++ Yatzy.py
@@ -22,17 +22,27 @@ class DIE:
         else:
             print("Die {}, value: {}".format(self.die_name, self.value))
 
+class POINTS:
+    def __init__(self, type_:str):         #Endast till för att kunna göra changeability, antagligen ineffektivt, se senare.
+        self.changeability = True
+        self.type_=type_
+        
+    def give(self):
+        for i in self.categories:
+            
+            
+
 class PLAYER:
     
     def __init__(self):
         self.dices = np.array([DIE("1"), DIE("2"), DIE("3"), DIE("4"), DIE("5")])    #så att jag kan kalla på't och har dem fysiskt
-        self.points_siffror = {"ettor":0, "tvåor":0, "treor":0, "fyror":0, "femmor":0, "sexor":0} # en dictionary kan nog funka bra
-        self.points_extra = {"tretal":0, "fyrtal":0, "liten stege":0, "stor stege":0, "kåk":0, "chans":0, "yatzy":0}
+        self.categories = {POINTS("ettor"):0, POINTS("tvåor"):0, POINTS("treor"):0, POINTS("fyror"):0, POINTS("femmor"):0, POINTS("sexor"):0
+                            POINTS("tretal"):0, POINTS("fyrtal"):0, POINTS("liten stege"):0, POINTS("stor stege"):0, POINTS("kåk"):0, POINTS("chans"):0, POINTS("yatzy"):0}
         
     def ask(self):
         """Asks the player to input a list of numbers, locks/unlocks the corresponding dice."""    
         
-        x = list(map(int, input("\nEnter which dice to lock, 1 - 5, with spaces between: ").strip().split()))[:5]
+        x = list(map(int, input("\nSkriv vilka tärningar du vill låsa, 1 - 5, med mellanslag : ").strip().split()))[:5]
         
         for element in x:
             self.dices[element - 1].hold = not(self.dices[element - 1].hold)
@@ -45,13 +55,24 @@ class PLAYER:
         self.reset()
             
     def points(self):
-        if self.dices[0].value == self.dices[1].value and self.dices[0].value == self.dices[2].value 
-        and self.dices[0].value == self.dices[3].value and self.dices[0].value == self.dices[4].value:
-            self.points_extra = 
-                
+        self.which = input("Kategori? Ettor, tvåor, kåk, o.s.v").lower()
+        
+        if self.categories().self.chageability == True
+            
+            if 
+            
+            """else: 
+                self.categories[self.which] = 0 and self.categories[self.which].self.changeability = False"""
+        
+        else:
+            print("choose another")
+            points()
+        
     def reset_dice(self):
+        
         for i in self.dices:
-            self.dices[i].hold = False
+            self.dices[i].hold = False 
+            
         self.ask()
     
 class START:
@@ -60,6 +81,11 @@ class START:
         self.antal()
         self.getname()
         self.players = [PLAYER() for _ in range(self.antal_spelare)]
+    
+    def new_round(self):
+        self.reset_dice()
+        for i in self.categories:
+            self.categories(i).self.changeability = True
     
     def antal(self):
         print("Hur många spelare?")
